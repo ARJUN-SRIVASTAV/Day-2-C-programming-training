f5cd6ed71250661e3bc072f4026690135a1ec4bd
ARJUN-SRIVASTAV <164508907+ARJUN-SRIVASTAV@users.noreply.github.com>
2024-04-04 23:35:16 +0530

    Update series.c

---
+++ series.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main ()
+{
+    int a=9,b;
+    scanf("%d",&b);
+    for(int i=0; i<=b;i++)
+    {
+       
+        for(int j=0;j<=i;j++)
+        {
+            printf("%d",a); 
+        
+            
+      }  if(i==b)
+        { break;
+        }else
+        {
+        
+         printf("+");
+            
+        }
+}
+return 0;
+
+}
